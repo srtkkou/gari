211270368248b5eda958eadc8eebef3e4916497c
srtkkou <srtkkou@outlook.jp>
2026-04-20 19:17:24 +0900

    [feat] Added scanNullTime() to store DB Time value in Cell struct.

--- cell.go
+++ cell.go
@@ -4,10 +4,9 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	//	"slices"
-	//	"strconv"
 	"strings"
-	//	"github.com/goark/errs"
+
+	"github.com/goark/errs"
 )
 
 type (
@@ -22,6 +21,8 @@ type (
 var (
 	// Cell kind error
 	ErrCellKind = errors.New("orm.ErrCellKind")
+	// Scan error
+	ErrScan = errors.New("orm.ErrScan")
 )
 
 // Create new Cell struct.
@@ -33,24 +34,7 @@ func newCell(col *Column) *Cell {
 	return &c
 }
 
-/*
-// 列の新規作成
-func newColumn(name string) *Column {
-	return &Column{
-		name:         name,
-		attrName:     snakeToUpperCamelCase(name),
-		jsonKey:      snakeToLowerCamelCase(name),
-		kind:         "",
-		size:         -1,
-		allowNull:    false,
-		value:        []byte{},
-		defaultValue: []byte{},
-		rules:        make([]validation.Rule, 0),
-	}
-}
-*/
-
-// 文字列化
+// String
 func (c Cell) String() string {
 	var b strings.Builder
 	b.WriteString(`{"column":`)
@@ -59,25 +43,49 @@ func (c Cell) String() string {
 	return b.String()
 }
 
+// Scan value into Cell struct.
 func (c *Cell) Scan(value any) error {
 	switch c.column.kind {
 	case kindString:
 		return c.scanNullString(value)
+	case kindTime:
+		return c.scanNullTime(value)
 	default:
 		return ErrCellKind
 	}
 }
 
+// Scan sql.NullString type.
 func (c *Cell) scanNullString(value any) error {
 	ns := sql.NullString{}
 	err := ns.Scan(value)
 	if err != nil {
-		fmt.Printf("ScanError=%v\n", err)
+		err = errs.Wrap(ErrScan, errs.WithCause(err),
+			errs.WithContext("value", value))
 		return err
 	}
 	fmt.Printf("NullString:Valid=%t,String=%s\n", ns.Valid, ns.String)
 	// Store value
 	blob, err := encodeNullString(ns)
+	if err != nil {
+		return err
+	}
+	c.value = blob
+	return nil
+}
+
+// Scan sql.NullTime type.
+func (c *Cell) scanNullTime(value any) error {
+	nt := sql.NullTime{}
+	err := nt.Scan(value)
+	if err != nil {
+		err = errs.Wrap(ErrScan, errs.WithCause(err),
+			errs.WithContext("value", value))
+		return err
+	}
+	fmt.Printf("NullTime:Valid=%t,Time=%v\n", nt.Valid, nt.Time)
+	// Store value
+	blob, err := encodeNullTime(nt)
 	if err != nil {
 		return err
 	}
