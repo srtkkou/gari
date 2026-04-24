dd6c092757772af7c2acdd86ab365610a7c583ce
srtkkou <srtkkou@outlook.jp>
2026-04-25 00:15:29 +0900

    [feat] Added Migrate() on sqlite test.
    * Uncommented Code to define samples table.

--- sqlite_test.go
+++ sqlite_test.go
@@ -1,6 +1,7 @@
 package gari
 
 import (
+	"context"
 	"database/sql"
 	"testing"
 
@@ -9,23 +10,24 @@ import (
 )
 
 func TestSqlite(t *testing.T) {
-	/*
-		// Define objects table.
-		gari, err := New()
-		require.NoError(t, err)
-		objectsTable, err := gari.DefineTable("objects").
-			AddInt64Column("num", func(col *Column) {
-				col.SetDefaultInt64(0)
-			}).
-			AddStringColumn("text", func(col *Column) {
-				col.SetSize(255)
-				col.SetDefaultString("defaultString")
-			}).
-			Build()
-		require.NoError(t, err)
-	*/
+	// Define samples table.
+	gari, err := New()
+	require.NoError(t, err)
+	sampleTable := gari.Table("samples").
+		AddInt64Column("num", func(col *Column) {
+			col.SetDefaultInt64(0)
+		}).
+		AddStringColumn("text", func(col *Column) {
+			col.SetSize(255)
+			col.SetDefaultString("defaultString")
+		}).
+		MustDefine()
 	// Open SQLite
 	db, err := sql.Open("sqlite", ":memory:?_foreign_keys(1)")
 	require.NoError(t, err)
 	defer db.Close()
+	// Migrate.
+	ctx := context.Background()
+	err = sampleTable.Migrate(ctx, db)
+	require.NoError(t, err)
 }
