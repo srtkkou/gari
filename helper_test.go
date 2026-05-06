76385a1f7b19cfc4c38c627f76d0a22bbe15c431
srtkkou <srtkkou@outlook.jp>
2026-05-06 20:44:29 +0900

    [feat] Added helper to send logging messages to t.Log().

--- helper_test.go
+++ helper_test.go
@@ -2,6 +2,7 @@ package gari
 
 import (
 	"database/sql"
+	"testing"
 	"time"
 )
 
@@ -15,4 +16,11 @@ func time20111213() time.Time {
 	return time.Date(
 		2011, time.December, 13, 14, 15, 16, 789000000, time.UTC,
 	)
+}
+
+// Define logger.
+func tLog(t *testing.T) func(string, ...any) {
+	return func(msg string, args ...any) {
+		t.Log(append([]any{msg}, args...))
+	}
 }
