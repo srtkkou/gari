6fba9ee612fdfecf1cd315c6d13f1849ed9f2a86
srtkkou <srtkkou@outlook.jp>
2026-05-10 21:40:29 +0900

    [feat] Fixed dialect funcs to use StringWriter.

--- dialect.go
+++ dialect.go
@@ -1,8 +1,16 @@
 package gari
 
+import (
+	"io"
+)
+
 type (
 	Dialect interface {
-		QuerySuffix() string
-		BindVar(i int) string
+		QuoteString(w io.StringWriter, str string)
+		QuoteTable(w io.StringWriter, table string)
+		QuoteColumn(w io.StringWriter, column string)
+		QuoteAlias(w io.StringWriter, alias string)
+		QuerySuffix(w io.StringWriter)
+		BindVar(w io.StringWriter, i int)
 	}
 )
--- dialect_sqlite.go
+++ dialect_sqlite.go
@@ -1,14 +1,46 @@
 package gari
 
+import (
+	"io"
+)
+
 type (
 	SqliteDialect struct {
 	}
 )
 
-func (d SqliteDialect) QuerySuffix() string {
-	return ";"
+// Single quote.
+func (d SqliteDialect) QuoteString(w io.StringWriter, str string) {
+	w.WriteString(`'`)
+	w.WriteString(str)
+	w.WriteString(`'`)
 }
 
-func (d SqliteDialect) BindVar(i int) string {
-	return "?"
+// Double quote.
+func (d SqliteDialect) QuoteTable(w io.StringWriter, table string) {
+	w.WriteString(`"`)
+	w.WriteString(table)
+	w.WriteString(`"`)
+}
+
+// Double quote.
+func (d SqliteDialect) QuoteColumn(w io.StringWriter, column string) {
+	w.WriteString(`"`)
+	w.WriteString(column)
+	w.WriteString(`"`)
+}
+
+// Double quote.
+func (d SqliteDialect) QuoteAlias(w io.StringWriter, alias string) {
+	w.WriteString(`"`)
+	w.WriteString(alias)
+	w.WriteString(`"`)
+}
+
+func (d SqliteDialect) QuerySuffix(w io.StringWriter) {
+	w.WriteString(`;`)
+}
+
+func (d SqliteDialect) BindVar(w io.StringWriter, i int) {
+	w.WriteString(`?`)
 }
